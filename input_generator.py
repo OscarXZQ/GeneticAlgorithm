456a2619eebdee9065c30a0326821a5636731af6
Kelvin Lee <kelvinlee@berkeley.edu>
2020-11-18 00:42:30 -0800

    created input generator

--- input_generator.py
+++ input_generator.py
@@ -1,1 +1,14 @@
+import random
+
+def generate(n, s_max):
+    input = str(n) + "\n" + str(s_max) + "\n"
+    for i in range(n - 1):
+        for j in range(i + 1, n):
+            happiness = round(random.uniform(0, 100), 3)
+            stress = round(random.uniform(0, 100), 3)
+            input += str(i) + " " + str(j) + " " + str(happiness) + " " + str(stress) + "\n"
+
+    with open(str(n)+".in", 'w') as fo:
+        fo.write(input)
+
 
