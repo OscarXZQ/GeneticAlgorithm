71a17aba712c32dec72ddd25dfe9c654fc84716e
Tianchen Liu <tianchenliu@berkeley.edu>
2020-11-18 19:42:57 -0800

    wrote greedy

--- solver_greedy.py
+++ solver_greedy.py
@@ -1,7 +1,48 @@
 import networkx as nx
 from parse import read_input_file, write_output_file
 from utils import is_valid_solution, calculate_happiness
+from utils import convert_dictionary
 import sys
+import random
+
+def try_partition_into_k_groups(k, G, s):
+    if k == 1:
+        D = {}
+        for i in range(len(G)):
+            D[i] = 0
+        if is_valid_solution(D, G, s, k):
+            return True, D
+        else:
+            return False, None
+
+    # Greedy MAX-K-CUT, could enhance by using better MAX-K-CUT algorithms
+    seq = list(G.nodes())
+    random.shuffle(seq)
+    D = {} # student to room
+    group_list = [ [] for _ in range(k)] # room to student
+    
+    for i in range(k):
+        D[seq[i]] = i
+        group_list[i].append(seq[i])
+    for i in range(k, len(seq)):
+        cur_node = seq[i]
+        # print(type(cur_node), cur_node)
+        mini = float('inf')
+        cur_group = None
+        for group_num, group in enumerate(group_list):
+            stress = sum(G[i][other_person]['stress'] for other_person in group if G.has_edge(i, other_person))
+            if stress < mini:
+                mini = stress
+                cur_group = group_num
+        assert cur_group != None
+        D[cur_node] = cur_group
+        group_list[cur_group].append(cur_node)
+    if (is_valid_solution(D, G, s, k)):
+        return True, D
+    else:
+        return False, None
+
+
 
 
 def solve(G, s):
@@ -15,21 +56,32 @@ def solve(G, s):
     """
 
     # TODO: your code here!
-    pass
+    #pass
+    # n = G.size()
+    n = len(G)
+    for k in range(1, n+1):
+        status, D = try_partition_into_k_groups(k, G, s)
+        if status:
+            return D, k
+    assert False
 
+
+
+
+# Calculate the total stress in the room
 
 # Here's an example of how to run your solver.
 
 # Usage: python3 solver.py test.in
 
-# if __name__ == '__main__':
-#     assert len(sys.argv) == 2
-#     path = sys.argv[1]
-#     G, s = read_input_file(path)
-#     D, k = solve(G, s)
-#     assert is_valid_solution(D, G, s, k)
-#     print("Total Happiness: {}".format(calculate_happiness(D, G)))
-#     write_output_file(D, 'out/test.out')
+if __name__ == '__main__':
+    assert len(sys.argv) == 2
+    path = sys.argv[1]
+    G, s = read_input_file(path)
+    D, k = solve(G, s)
+    assert is_valid_solution(D, G, s, k)
+    print("Total Happiness: {}".format(calculate_happiness(D, G)))
+    write_output_file(D, 'out/test.out')
 
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
