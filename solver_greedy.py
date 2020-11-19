86bc74676bf8c683dc0138b097ab3a28a0df01a9
Tianchen Liu <tianchenliu@berkeley.edu>
2020-11-18 20:51:04 -0800

    improved greedy alg

--- solver_greedy.py
+++ solver_greedy.py
@@ -5,43 +5,53 @@ from utils import convert_dictionary
 import sys
 import random
 
+num_trials = 100
+
 def try_partition_into_k_groups(k, G, s):
+    '''
+    Try partitioning the 
+    '''
     if k == 1:
         D = {}
         for i in range(len(G)):
             D[i] = 0
         if is_valid_solution(D, G, s, k):
-            return True, D
+            return D
         else:
-            return False, None
+            return None
 
-    # Greedy MAX-K-CUT, could enhance by using better MAX-K-CUT algorithms
-    seq = list(G.nodes())
-    random.shuffle(seq)
-    D = {} # student to room
-    group_list = [ [] for _ in range(k)] # room to student
-    
-    for i in range(k):
-        D[seq[i]] = i
-        group_list[i].append(seq[i])
-    for i in range(k, len(seq)):
-        cur_node = seq[i]
-        # print(type(cur_node), cur_node)
-        mini = float('inf')
-        cur_group = None
-        for group_num, group in enumerate(group_list):
-            stress = sum(G[i][other_person]['stress'] for other_person in group if G.has_edge(i, other_person))
-            if stress < mini:
-                mini = stress
-                cur_group = group_num
-        assert cur_group != None
-        D[cur_node] = cur_group
-        group_list[cur_group].append(cur_node)
-    if (is_valid_solution(D, G, s, k)):
-        return True, D
-    else:
-        return False, None
+    maxi = -1
+    ret = None
+    for _ in range(num_trials):
+        # Greedy MAX-K-CUT, could enhance by using better MAX-K-CUT algorithms
+        seq = list(G.nodes())
+        random.shuffle(seq)
+        D = {} # student to room
+        group_list = [ [] for _ in range(k)] # room to student
+        
+        for i in range(k):
+            D[seq[i]] = i
+            group_list[i].append(seq[i])
 
+        for i in range(k, len(seq)):
+            cur_node = seq[i]
+            # print(type(cur_node), cur_node)
+            mini = float('inf')
+            cur_group = None
+            for group_num, group in enumerate(group_list):
+                stress = sum(G[i][other_person]['stress'] for other_person in group if G.has_edge(i, other_person))
+                if stress < mini:
+                    mini = stress
+                    cur_group = group_num
+            assert cur_group != None
+            D[cur_node] = cur_group
+            group_list[cur_group].append(cur_node)
+        if (is_valid_solution(D, G, s, k)):
+            cur_val = calculate_happiness(D, G)
+            if cur_val > maxi:
+                maxi = cur_val
+                ret = D
+    return ret
 
 
 
@@ -60,8 +70,8 @@ def solve(G, s):
     # n = G.size()
     n = len(G)
     for k in range(1, n+1):
-        status, D = try_partition_into_k_groups(k, G, s)
-        if status:
+        D = try_partition_into_k_groups(k, G, s)
+        if D is not None:
             return D, k
     assert False
 
