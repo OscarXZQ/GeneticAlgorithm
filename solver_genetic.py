7b680a9d8f248b5de144be9cf7ced1ddc9916b72
Oscar Xu <oscar.xzq@berkeley.edu>
2020-12-01 08:11:04 -0800

    add large3

--- solver_genetic.py
+++ solver_genetic.py
@@ -252,27 +252,13 @@ def solve(G, s):
 
 # Here's an example of how to run your solver.
 
-
-
 # Usage: python3 solver.py test.in
-
-if __name__ == '__main__':
-    # assert len(sys.argv) == 2
-    # path = sys.argv[1]
-    path = 'inputs/large/large-27.in'
-    G, s = read_input_file(path)
-    D, k = solve(G, s)
-    assert is_valid_solution(D, G, s, k)
-    print("Total Happiness: {}".format(calculate_happiness(D, G)))
-    write_output_file(D, 'outputs/large/large-27-try.out')
-
-
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 compare = []
 if __name__ == '__main__':
-    inputs = glob.glob('inputs/large1/*') #change large1     dzj: large2 lzw: large3
+    inputs = glob.glob('inputs/large3/*') #change large1     dzj: large2 lzw: large3
     inputs = iter(inputs)
-    with pymp.Parallel(4) as p:
+    with pymp.Parallel(8) as p:
         for input_path in p.iterate(inputs):
             print(input_path)
             cur_file = basename(normpath(input_path))[:-3]
