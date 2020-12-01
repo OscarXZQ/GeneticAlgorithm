3f564e8fb3a1cbcc2e37bf8fafd6f8f8c253afea
Jason Ding <zhoujie.ding@berkeley.edu>
2020-12-01 10:48:53 +0800

    solver for large2

--- solver_genetic.py
+++ solver_genetic.py
@@ -255,7 +255,7 @@ def solve(G, s):
 
 
 # Usage: python3 solver.py test.in
-
+"""
 if __name__ == '__main__':
     # assert len(sys.argv) == 2
     # path = sys.argv[1]
@@ -265,18 +265,18 @@ if __name__ == '__main__':
     assert is_valid_solution(D, G, s, k)
     print("Total Happiness: {}".format(calculate_happiness(D, G)))
     write_output_file(D, 'outputs/large/large-27-try.out')
-
+"""
 
 # For testing a folder of inputs to create a folder of outputs, you can use glob (need to import it)
 compare = []
 if __name__ == '__main__':
-    inputs = glob.glob('inputs/large1/*') #change large1     dzj: large2 lzw: large3
+    inputs = glob.glob('inputs/large2/*') #change large1     dzj: large2 lzw: large3
     inputs = iter(inputs)
     with pymp.Parallel(4) as p:
         for input_path in p.iterate(inputs):
             print(input_path)
             cur_file = basename(normpath(input_path))[:-3]
-            output_path = 'outputs/large_genetic/' + cur_file + '.out'
+            output_path = 'outputs/large2/' + cur_file + '.out'
             G, s = read_input_file(input_path)
             D = solve(G, s)
             # D, k = solve(G, s)
@@ -285,5 +285,5 @@ if __name__ == '__main__':
             print("Final Happiness:", final_happiness)
             write_output_file(D, output_path)
             compare.append(str(cur_file) + str(final_happiness))
-        write_output_file(compare, "outputs/medium_genetic_nov29_log.txt")
+        write_output_file(compare, "outputs/large2.txt")
 
